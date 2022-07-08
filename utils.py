f5bc8d0989649268356d62150b5647be4d644cdc
johnarban <jaffagate1@gmail.com>
2022-07-08 09:56:47 -0400

    colormap changes

--- utils.py
+++ utils.py
@@ -1998,7 +1998,7 @@ def plot_walkers(sampler, limits=None, bad=None):
 # Make it scale properly
 # How does matplotlib
 # scaling work
-def combine_cmap(cmaps, lower, upper, log = False,name="custom", N=None, register=True, norm=False):
+def combine_cmap_val(cmaps,values=None, lower=None, upper=None, log = False,name="custom", N=None, register=True, norm=False):
     """
     colormaps : a list of N matplotlib colormap classes
     lower : the lower limits for each colormap: array or tuple
@@ -2006,6 +2006,15 @@ def combine_cmap(cmaps, lower, upper, log = False,name="custom", N=None, registe
     log   : Do you want to plot logscale. This will create
             a color map that is usable with LogNorm()
     """
+
+    if (values is None) & (lower is None) & (upper is None):
+        values = np.linspace(0,1,len(cmaps))
+
+    if (upper is None) & (lower is None):
+        upper = values[1:]
+        lower = values[:-1]
+
+
     if log:
         upper = [np.log10(i / lower[0]) for i in upper]
         lower = [np.log10(i / lower[0]) for i in lower]
@@ -2055,83 +2064,48 @@ def combine_cmap(cmaps, lower, upper, log = False,name="custom", N=None, registe
 
 
 
-def custom_cmap(colormaps, lower, upper, log=(0, 0)):
-    """
-    colormaps : a list of N matplotlib colormap classes
-    lower : the lower limits for each colormap: array or tuple
-    upper : the upper limits for each colormap: array or tuple
-    log   : Do you want to plot logscale. This will create
-            a color map that is usable with LogNorm()
-    """
-    if isinstance(log, tuple):
-        for lg in log:
-            if lg:
-                upper = [np.log10(i / lower[0]) for i in upper]
-                lower = [np.log10(i / lower[0]) for i in lower]
-                norm = upper[-1:][0]
-            else:
-                lower = lower
-                upper = upper
-                norm = upper[-1:][0]
-    elif log:
-        upper = [np.log10(i / lower[0]) for i in upper]
-        lower = [np.log10(i / lower[0]) for i in lower]
-        norm = upper[-1:][0]
-    else:
-        lower = lower
-        upper = upper
-        norm = upper[-1:][0]
-
-    for ic, c in enumerate(colormaps):
-        if isinstance(c, str):
-            colormaps[ic] = mpl.cm.get_cmap(c)
-
-    cdict = {"red": [], "green": [], "blue": []}
-
-    for color in ["red", "green", "blue"]:
-        for j, col in enumerate(colormaps):
-            # print j,col.name,color
-            x = [i[0] for i in col._segmentdata[color]]
-            y1 = [i[1] for i in col._segmentdata[color]]
-            y0 = [i[2] for i in col._segmentdata[color]]
-            x = [(i - min(x)) / (max(x) - min(x)) for i in x]
-            x = [((i * (upper[j] - lower[j])) + lower[j]) / norm for i in x]
-            if (j == 0) & (x[0] != 0):
-                x[:0], y1[:0], y0[:0] = [0], [y1[0]], [y0[0]]
-            for i in range(len(x)):  # first x needs to be zero
-                cdict[color].append((x[i], y1[i], y0[i]))
-
-    return colors.LinearSegmentedColormap("my_cmap", cdict)
-
-
-
-def combine_cmap(cmaps=['viridis','turbo'],split_fracs=0.5):
+def combine_cmap(cmaps=None,split_fracs=None):
     """
     split a colormap at a certain location
+    cmaps - a list of N matplotlib colormap classes (defaults to viridis)
+    split_fracs - list of fractions to split at (length = len(cmaps)-1)
+                  defaults to evenly distributing the colormaps
 
-    split - where along the colormap will be our split point
-            by default this split point is put in the middle
-            of the values
-    vmin  value for colorbar to start at: should max vim in
-            plotting command
-    vmaxs  (splitvalue,vmax) - where to start the second segment
-            of the color map. cmap(split) will be located
-            at valeu=splitvalue
-    vplit = instead of giving vmin,vmax,a you can split it at a
-            value between 0,1.
-    log     doesn't do what anyone would think, don't recommend using
 
     """
 
+    if cmaps is None:
+        cmaps = [mpl.cm.viridis]
+
+    nmaps = len(cmaps)
 
 
-    ncolors  = 1024
+    if split_fracs is None:
+        # distribute evenly between 0 and 1
+        split_fracs = np.diff(np.linspace(0, 1, nmaps + 1))
+    else:
+        split_fracs = np.atleast_1d(split_fracs)
+        if len(split_fracs) == nmaps - 1:
+            split_fracs = np.diff(np.r_[0, split_fracs, 1])
+        elif len(split_fracs) > nmaps - 1:
+            split_fracs = np.diff(np.r_[0, split_fracs[:nmaps-1], 1])
+        else:
+            split_fracs = np.diff(np.linspace(0, 1, nmaps + 1))
 
-    cols1 = mpl.cm.get_cmap(cmaps[0])(np.linspace(0, 1, int(ncolors * split_fracs)))
-    cols2 = mpl.cm.get_cmap(cmaps[1])(np.linspace(0, 1, int(ncolors * (1 - split_fracs))))
+    ncolors  = 256 * nmaps
+
+    # create a list of colors
+    cols = []
+    for i,c in enumerate(cmaps):
+        cmap = mpl.cm.get_cmap(c)
+        ncolor = int(ncolors * split_fracs[i])
+        cols.append(cmap(np.linspace(0, 1, ncolor)))
+
+    # cols1 = mpl.cm.get_cmap(cmaps[0])(np.linspace(0, 1, int(ncolors * split_fracs)))
+    # cols2 = mpl.cm.get_cmap(cmaps[1])(np.linspace(0, 1, int(ncolors * (1 - split_fracs))))
 
     # Combine them and build a new colormap:
-    return mpl.colors.ListedColormap(np.vstack( (cols1,cols2) ))
+    return mpl.colors.ListedColormap(np.vstack(cols))
 
 
 
@@ -2199,6 +2173,71 @@ def split_cmap(cmapn='viridis',split=0.5,vmin=0, vmaxs=(.5,1),vstep=None,
         return cmap,mpl.colors.Normalize(vmin=vmin,vmax=vmaxs[1])
 
     return cmap
+
+
+def combine_colormaps(colormaps, limits, log=False):
+    """
+    colormaps : a list of N matplotlib colormap classes
+    lower : the lower limits for each colormap: array or tuple
+    upper : the upper limits for each colormap: array or tuple
+    log   : Do you want to plot logscale. This will create
+            a color map that is usable with LogNorm()
+    """
+
+    limits
+
+    if isinstance(log, tuple):
+        if len(log) == len(limits)-1:
+            if np.any(log):
+                for lg in log:
+                    if lg:
+                        upper = [np.log10(i / lower[0]) for i in upper]
+                        lower = [np.log10(i / lower[0]) for i in lower]
+                        norm = upper[-1:][0]
+                    else:
+                        lower = lower
+                        upper = upper
+                        norm = upper[-1:][0]
+        else:
+            raise ValueError('log must be a tuple of length len(limits)-1')
+    elif log:
+        upper = [np.log10(i / lower[0]) for i in upper]
+        lower = [np.log10(i / lower[0]) for i in lower]
+        norm = upper[-1:][0]
+    else:
+        lower = lower
+        upper = upper
+        norm = upper[-1:][0]
+
+    for ic, c in enumerate(colormaps):
+        if isinstance(c, str):
+            colormaps[ic] = mpl.cm.get_cmap(c)
+
+    cdict = {"red": [], "green": [], "blue": []}
+
+
+    for j, col in enumerate(colormaps):
+        if isinstance(col, mpl.colors.LinearSegmentedColormap):
+            segmentdata = col._segmentdata
+            for color in ['red','green','blue']:
+                c  = segmentdata[color]
+                x  = [i[0] for i in c]
+                y1 = [i[1] for i in c]
+                y0 = [i[2] for i in c]
+
+                x = ((x * (upper[j] - lower(j))) + lower[j]) / norm
+                cdict[color].append((x, y0, y1))
+        else:
+            x = np.linspace(0, 1, col.N)
+            xcol = ((x * (upper[j] - lower(j))) + lower[j]) / norm
+            r,g,b = col.colors.T # returns r,g,b,a
+
+            cdict["red"].append(  (xcol, r, r))
+            cdict["green"].append((xcol, g, g))
+            cdict["blue"].append( (xcol, b, b))
+
+    return colors.LinearSegmentedColormap("my_cmap", cdict)
+
 
 def plot_2dhist(
     X,
